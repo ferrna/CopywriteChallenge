2f79635f4f0fb6cb06fee4e22940e31d6d9795ef
Fernando Arriondo <arriondovfernando@gmail.com>
2022-07-28 20:53:53 -0300

    make it better

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ import { FETCH_TEXT_TRANSFORM } from "../consts";
 
 export const getTextTransform = function (text) {
   return async function (dispatch) {
-    return await axios(`http://localhost:3001/iecho?text=${text}`).then((json) =>
+    return await axios(`https://node-express-copywrite.herokuapp.com/iecho?text=${text}`).then((json) =>
       dispatch({ type: FETCH_TEXT_TRANSFORM, payload: json.data })
     );
   };
