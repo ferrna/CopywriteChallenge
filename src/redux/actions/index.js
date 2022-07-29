470c4034450c9c79dce9570935554af5ba0543ef
Fernando Arriondo <arriondovfernando@gmail.com>
2022-07-28 23:57:32 -0300

    hotfix

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,12 @@
 import { FETCH_TEXT_TRANSFORM } from "../consts";
 import httpService from "../../services/httpService";
+import axios from "axios"
 
 const service = new httpService()
 
 export const getTextTransform = function (text) {
   return async function (dispatch) {
-    return await service.get(`iecho?text=${text}`).then((json) =>
+    return await axios(`https://node-express-copywrite.herokuapp.com/iecho?text=${text}`).then((json) =>
       dispatch({ type: FETCH_TEXT_TRANSFORM, payload: json.data })
     );
   };
